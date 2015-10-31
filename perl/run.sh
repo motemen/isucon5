4ce757897f5f7ed615a80fad3ff347ec8a8aa4f0
wtatsuru <wtatsuru@hatena.ne.jp>
2015-10-31 13:08:16 +0900

    Revert "Unix domain socket 使うぞ"
    This reverts commit f3fbbc72de48b9b674656faba237f2f3e6ca7fb4.

--- perl/run.sh
+++ perl/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec /home/isucon/env.sh carton exec -- plackup -s Gazelle --path /dev/shm/app.sock --workers 4 --max-reqs-per-child 100000 --min-reqs-per-child 80000 app.psgi
+exec /home/isucon/env.sh carton exec -- plackup -s Gazelle -p 8080 --workers 4 --disable-keepalive app.psgi
